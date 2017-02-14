7cbcfc7a8dbe5419d7ab4d4e1028343c97309dfb
Justin <me@justinlam.ca>
2017-02-14 15:48:24 -0500

    Update documentation of PropertyStore interface to indicate static / instance methods;

--- src/interfaces/propertyStore.js
+++ src/interfaces/propertyStore.js
@@ -15,6 +15,7 @@ Flybits.interface.PropertyStore = {
    * Retrieves property value based on key.
    * @function
    * @memberof Flybits.interface.PropertyStore
+   * @instance
    * @param key Key by which to fetch the requested property.
    * @return {external:Promise<string,Flybits.Validation>} Promise that resolves with property store value unless a problem occurs.
    */
@@ -24,6 +25,7 @@ Flybits.interface.PropertyStore = {
    * Converts context value object into the server expected format.
    * @function
    * @memberof Flybits.interface.PropertyStore
+   * @instance
    * @param {string} key Key by which to store provided `value`
    * @param {string} value Value that is to be stored based on provided `key`
    * @return {external:Promise<undefined,Flybits.Validation>} Promise that resolves without value if value has been successfully stored based on specified key.
@@ -34,6 +36,7 @@ Flybits.interface.PropertyStore = {
    * Completely remove key and value from property store.
    * @function
    * @memberof Flybits.interface.PropertyStore
+   * @instance
    * @param {string} key Key by which to store provided `value`
    * @return {external:Promise<undefined,Flybits.Validation>} Promise that resolves without value if key and value have been successfully removed based on specified key.
    */
