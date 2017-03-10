59eaed78c331099d188abc9c95057b146ab63b4c
Justin <me@justinlam.ca>
2017-03-10 17:30:26 -0500

    Added tests for analytics event persistent storage class;

--- test/analytics/managerInitialization.js
+++ test/analytics/managerInitialization.js
@@ -151,4 +151,146 @@ describe('Analytics Manager Initialization', function(){
       (Flybits.analytics.Manager._uploadChannel instanceof Flybits.analytics.UploadChannel).should.be.true();
     })
   });
+
+  describe('Persistence store functions', function(){
+    beforeEach(function(done){
+      mockery.enable({
+        useCleanCache: true,
+        warnOnReplace: false,
+        warnOnUnregistered: false
+      });
+
+      require('../../index.js');
+      global.Flybits = window.Flybits;
+      Flybits.initObj().then(function(){
+        sinon.stub(Flybits.analytics.Manager,'startReporting',function(){
+          return Promise.resolve();
+        });
+        return Flybits.analytics.Manager.initialize();
+      }).then(function(){
+        done();
+      }).catch(function(e){
+        done(e);
+      });
+    });
+    afterEach(function(){
+      Flybits.analytics.Manager.startReporting.restore();
+      global.Flybits = sdkStub;
+      mockery.disable();
+    });
+
+    it('should be able to log and retrieve single event', function(done){
+      var evt = new Flybits.analytics.Event();
+      var store = Flybits.analytics.Manager._analyticsStore;
+      var stateSaveSpy = sinon.spy(Flybits.analytics.BrowserStore.prototype,'_saveState');
+
+      store.addEvent(evt).then(function(){
+        stateSaveSpy.should.be.calledOnce();
+        return store.getEvent(evt._tmpID);
+      }).then(function(e){
+        e.should.be.eql(evt);
+        done();
+      }).catch(function(e){
+        done(e);
+      }).then(function(){
+        stateSaveSpy.restore();
+      });
+    });
+
+    it('should be able to clear single event', function(done){
+      var store = Flybits.analytics.Manager._analyticsStore;
+
+      var evt1 = new Flybits.analytics.Event();
+      var evt2 = new Flybits.analytics.Event();
+      var evt3 = new Flybits.analytics.Event();
+
+      Promise.settle([
+        store.addEvent(evt1),
+        store.addEvent(evt2),
+        store.addEvent(evt3)
+      ]).then(function(){
+        return store.clearEvents([evt1._tmpID]);
+      }).then(function(){
+        return Promise.settle([
+          store.getEvent(evt1._tmpID),
+          store.getEvent(evt2._tmpID),
+          store.getEvent(evt3._tmpID),
+        ]);
+      }).then(function(resArr){
+        resArr[0].status.should.be.exactly('resolved');
+        should(resArr[0].result).be.undefined();
+        resArr[1].status.should.be.exactly('resolved');
+        resArr[1].result.should.be.eql(evt2);
+        resArr[2].status.should.be.exactly('resolved');
+        resArr[2].result.should.be.eql(evt3);
+        done();
+      }).catch(function(e){
+        done(e);
+      });
+    });
+
+    it('should be able to clear multiple events', function(done){
+      var store = Flybits.analytics.Manager._analyticsStore;
+
+      var evt1 = new Flybits.analytics.Event();
+      var evt2 = new Flybits.analytics.Event();
+      var evt3 = new Flybits.analytics.Event();
+
+      Promise.settle([
+        store.addEvent(evt1),
+        store.addEvent(evt2),
+        store.addEvent(evt3)
+      ]).then(function(){
+        return store.clearEvents([evt1._tmpID,evt2._tmpID]);
+      }).then(function(){
+        return Promise.settle([
+          store.getEvent(evt1._tmpID),
+          store.getEvent(evt2._tmpID),
+          store.getEvent(evt3._tmpID),
+        ]);
+      }).then(function(resArr){
+        resArr[0].status.should.be.exactly('resolved');
+        should(resArr[0].result).be.undefined();
+        resArr[1].status.should.be.exactly('resolved');
+        should(resArr[1].result).be.undefined();
+        resArr[2].status.should.be.exactly('resolved');
+        resArr[2].result.should.be.eql(evt3);
+        done();
+      }).catch(function(e){
+        done(e);
+      });
+    });
+
+    it('should be able to clear all events', function(done){
+      var store = Flybits.analytics.Manager._analyticsStore;
+
+      var evt1 = new Flybits.analytics.Event();
+      var evt2 = new Flybits.analytics.Event();
+      var evt3 = new Flybits.analytics.Event();
+
+      Promise.settle([
+        store.addEvent(evt1),
+        store.addEvent(evt2),
+        store.addEvent(evt3)
+      ]).then(function(){
+        return store.clearAllEvents();
+      }).then(function(){
+        return Promise.settle([
+          store.getEvent(evt1._tmpID),
+          store.getEvent(evt2._tmpID),
+          store.getEvent(evt3._tmpID),
+        ]);
+      }).then(function(resArr){
+        resArr[0].status.should.be.exactly('resolved');
+        should(resArr[0].result).be.undefined();
+        resArr[1].status.should.be.exactly('resolved');
+        should(resArr[1].result).be.undefined();
+        resArr[2].status.should.be.exactly('resolved');
+        should(resArr[2].result).be.undefined();
+        done();
+      }).catch(function(e){
+        done(e);
+      });
+    });
+  });
 });
