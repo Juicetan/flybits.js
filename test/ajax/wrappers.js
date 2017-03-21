818b03db878ea75a7b9d2aaf44695969f2fadf63
Justin <me@justinlam.ca>
2017-03-20 21:57:23 -0400

    Added basic ajax wrapper tests;
    - tests if 200s will cause a promise resolution and anything >200 will cause promise rejection;
    - native fetch api will always resolve the promise if request itself succeeds

---
+++ test/ajax/wrappers.js
@@ -0,0 +1,80 @@
+var should = require('should');
+var mockery = require('mockery');
+var sinon = require('sinon');
+require('should-sinon');
+var fetchMock = require('fetch-mock');
+
+var sdkStub = {
+  cfg: {
+    store: {
+      SDKPROPS: 'flb.sdk.properties',
+      RESOURCEPATH: "./res/",
+      DEVICEID: 'flb_device',
+      USERTOKEN: 'flb_usertoken',
+      USERTOKENEXP: 'flb_usertoken_expiry'
+    },
+    analytics: {
+      CHANNELHOST: 'https://analytics.host.com',
+      CHANNELKEY: 'analyticschannelkey',
+      APPID: 'xxxx-xxxx-xxxx-xxxx-xxxx',
+      REPORTDELAY: 10
+    }
+  }
+};
+
+describe('Fetch Wrapper Verfication',function(){
+  before(function(){
+    mockery.enable({
+      useCleanCache: true,
+      warnOnReplace: false,
+      warnOnUnregistered: false
+    });
+
+    global.Flybits = require('../../index.js');
+  });
+  after(function(){
+    delete global.Flybits;
+    mockery.disable();
+  });
+
+  it('should resolve on 200s', function(done){
+    fetchMock.get('*', 200);
+    Flybits.util.Api.fetch('/test/').then(function(){
+      done();
+    }).catch(function(e){
+      done(e);
+    });
+    fetchMock.restore();
+  });
+
+  describe('should reject on >200s', function(){
+    it('should reject on 300s', function(){
+      fetchMock.get('*', 300);
+      Flybits.util.Api.fetch('/test/').then(function(e){
+        done(e);
+      }).catch(function(e){
+        done();
+      });
+      fetchMock.restore();
+    });
+    it('should reject on 400s', function(){
+      fetchMock.get('*', 400);
+      Flybits.util.Api.fetch('/test/').then(function(e){
+        done(e);
+      }).catch(function(e){
+        done();
+      });
+      fetchMock.restore();
+    });
+    it('should reject on 500s', function(){
+      fetchMock.get('*', 500);
+      Flybits.util.Api.fetch('/test/').then(function(e){
+        done(e);
+      }).catch(function(e){
+        done();
+      });
+      fetchMock.restore();
+    });
+  });
+
+});
